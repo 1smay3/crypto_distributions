c3563e1dd714cee1a49d8e0d7d064c2ed350daa5
spencermay <spencermayy@gmail.com>
2021-05-09 23:19:56 +0100

    date selection functionality

--- apps/distributions.py
+++ apps/distributions.py
@@ -18,8 +18,10 @@ relevant_names = available_pairs[['name','symbol']]
 relevant_names.rename(columns={'name':'label', 'symbol':'value'}, inplace=True)
 dropdown_dict = relevant_names.to_dict('records')
 
-print(dropdown_dict)
-
+all_dates = ['All', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+dates_dataframe = pd.DataFrame(all_dates, columns=['label'])
+dates_dataframe['value'] = pd.DataFrame(all_dates)
+date_dict = dates_dataframe.to_dict('records')
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
@@ -27,16 +29,22 @@ app.layout = html.Div([
     dcc.Dropdown(
         id='crypto_selector',
         options=dropdown_dict,
-        value='BTCUSD')])
+        value='BTCUSD'),
+    dcc.Dropdown(
+        id='date_selector',
+        options=date_dict,
+        value='All')
+])
 
 
 @app.callback(
     Output("graph", "figure"),
-    [Input("crypto_selector", "value")])
+    [Input("crypto_selector", "value"),
+     Input("date_selector", "value")])
 
-def display_color(crypto_selector):
+def display_color(crypto_selector, date_selector):
     data = r"/Users/spencermay/PycharmProjects/crypto_distributions/data/data_store/" + crypto_selector + ".pkl"
-    fig = total_histogram(data, 'All')
+    fig = total_histogram(data, date_selector)
     return fig
 
 app.run_server(debug=True)
