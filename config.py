e2576b99df99baa787da53d4fd7a08372612faee
Spencer May <69083772+1smay3@users.noreply.github.com>
2021-11-05 12:05:57 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 base_url ="https://financialmodelingprep.com/api/"
 version  = "v3/"
 
-api_key = "6a7bfe0b2162048feb12ab86dbd15631"
+api_key = ""
 
 # Add slash as os.join can only interpret '/' at the start of a path if it is the root of the directory
 project_root = os.path.abspath(os.path.dirname(__file__)) + "/"
